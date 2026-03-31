49bc358746ea1dc1a0966c95790b881ede31f7b0
yongkai1989 <yongkai.t@gmail.com>
2026-03-31 20:01:49 +0800

    Update page.tsx

--- app/dashboard/supervisor-update/page.tsx
+++ app/dashboard/supervisor-update/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useMemo, useState } from 'react';
 import Link from 'next/link';
-import { createBrowserSupabaseClient } from '../../lib/supabaseBrowser';
+import { createBrowserSupabaseClient } from '../../../lib/supabaseBrowser';
 
 type DashboardUser = {
   user_id?: string;
