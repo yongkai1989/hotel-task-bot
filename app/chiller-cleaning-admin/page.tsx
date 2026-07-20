197aea64160ee12ebc8288bbc87ff786f5ece7a4
yongkai1989 <yongkai.t@gmail.com>
2026-07-20 10:40:05 +0800

    Create page.tsx

---
+++ app/chiller-cleaning-admin/page.tsx
@@ -0,0 +1,8 @@
+import ChillerCleaningAdminPage from '../../components/ChillerCleaningAdminPage';
+
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+export default function ChillerCleaningStandaloneAdminPage() {
+  return <ChillerCleaningAdminPage />;
+}
