4db1a2a2dc0cd4e39b5bfac7fad6ce6bc8024772
yongkai1989 <yongkai.t@gmail.com>
2026-07-27 14:54:19 +0800

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -29,6 +29,7 @@ type DashboardUser = {
   can_access_linen_history?: boolean;
   can_access_daily_forms?: boolean;
   can_access_management_tasks?: boolean;
+  can_access_bank_in_cash?: boolean;
   can_access_admin_settings?: boolean;
   can_access_linen_admin?: boolean;
   can_access_lost_found?: boolean;
@@ -54,6 +55,7 @@ type DashboardUser = {
     | 'can_access_linen_history'
     | 'can_access_daily_forms'
     | 'can_access_management_tasks'
+    | 'can_access_bank_in_cash'
     | 'can_access_admin_settings'
     | 'can_access_linen_admin'
     | 'can_access_lost_found'
