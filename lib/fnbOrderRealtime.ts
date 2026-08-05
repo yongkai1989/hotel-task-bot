a618bcabc9ef1d8b059fd11af80fcc9de1e56993
yongkai1989 <yongkai.t@gmail.com>
2026-08-05 21:21:23 +0800

    Add realtime F&B helper (lib/fnbOrderRealtime.ts)

---
+++ lib/fnbOrderRealtime.ts
@@ -0,0 +1,5 @@
+export const FNB_ORDER_BROADCAST_CHANNEL = 'fnb-order-sync';
+export const FNB_ORDER_BROADCAST_EVENT = 'order-change';
+
+export type FnbOrderBroadcastEventType = 'INSERT' | 'UPDATE' | 'DELETE';
+
